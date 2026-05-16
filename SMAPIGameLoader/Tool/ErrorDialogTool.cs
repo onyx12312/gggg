77cebaccd98ca0f4dd87e14174f6119689f848a8
onyx12312 <rlqwpn2008@gmail.com>
2026-05-16 17:15:35 +0300

    Add SMAPIGameLoader/Tool/ErrorDialogTool.cs

---
+++ SMAPIGameLoader/Tool/ErrorDialogTool.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using SMAPIGameLoader.Tool;
+using System;
+using Xamarin.Essentials;
+
+namespace SMAPIGameLoader;
+
+internal static class ErrorDialogTool
+{
+    // you will got error when you alert.Show() & Finish() it
+    //example case
+    // android.view.WindowLeaked: Activity crc644389b739a03c2b33.SMAPIActivity has leaked window DecorView@fd80140[Error Dialog] that was originally added here
+    public static void Show(Exception exception, string title = "Exception Dialog")
+    {
+
+        if (exception is null)
+            return;
+
+        Console.WriteLine("try show error dialog: " + exception);
+
+        if (MainThread.IsMainThread == false)
+        {
+            TaskTool.RunMainThread(() =>
+            {
+                Show(exception, title);
+            });
+            return;
+        }
+
+        var dialog = new AlertDialog.Builder(ActivityTool.CurrentActivity);
+        var alert = dialog.Create();
+        alert.SetTitle(title);
+        alert.SetMessage(exception.ToString());
+        alert.SetButton("OK", (c, ev) =>
+        {
+            // Ok button click task  
+        });
+        alert.Show();
+
+        Clipboard.SetTextAsync(exception.ToString());
+    }
+}
